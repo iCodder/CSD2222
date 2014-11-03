68309d9d6861a31b844d9fd33c7dd31059aebc60
AS2556 <AS2556@PC-00105740.uni.mdx.ac.uk>
2014-11-03 13:50:32 +0000

    New class teststudent1

---
+++ src/teststudent1.java
@@ -0,0 +1,31 @@
+
+public class teststudent1 {
+
+	/**
+	 * @param args
+	 */
+	int c = 1;
+	int d = 3;
+	
+	public int getC() {
+		return c;
+	}
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public void setD(int d) {
+		this.d = d;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
