b468053b2ec5a67856cd48976b1d1c35e37cdaa3
MA2259 <MA2259@PC-00105830.uni.mdx.ac.uk>
2014-11-03 14:01:07 +0000

    Alex code

---
+++ src/teststudent2.java
@@ -0,0 +1,32 @@
+
+public class teststudent2 {
+	
+	int e = 11;
+	int r =12;
+	
+	
+	public int getE() {
+		return e;
+	}
+
+
+	public void setE(int e) {
+		this.e = e;
+	}
+
+
+	public int getR() {
+		return r;
+	}
+
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+
+	public static void main (String [] args){
+		
+	}
+
+}
